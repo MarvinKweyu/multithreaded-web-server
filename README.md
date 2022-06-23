d52b392117b3ab51e7c349889af2016099c01795
Marvin Kweyu <mkweyu1@gmail.com>
2022-06-23 12:47:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Hello Networking
+
+> A program to open a single threaded web server on the port provided
+
+#### Check for open ports
+
+```bash
+sudo ss -tunlp
+
+```
+
+Check on the port after access as one of the opne ports
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // iterate over connection attempts
+
+    for stream in listener.incoming() {
+        // use unwrap() to stop the program if an error happens
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // declare buffer on stack of 1024 bytes in size
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap(); // flush will prevent program from continuing until all bytes are written to connection
+                                 // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    } else {
+        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
